fa85a303c10a09e7c428f85c744981c33c202b10
Fabio Fernandes <fabio.fernandes@udroid.com.br>
2024-12-31 23:50:45 +0000

    Working rest service

---
+++ DrogonRestSample/src/DrogonRestSample.cpp
@@ -0,0 +1,19 @@
+﻿#include "DrogonRestSample.h"
+#include <drogon/drogon.h>
+#include <json/json.h> // jsoncpp
+
+int main() {
+    drogon::app().registerHandler("/hello",
+        [](const drogon::HttpRequestPtr& req,
+            std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+                Json::Value jsonResp;
+                jsonResp["message"] = "Hello, Drogon!";
+
+                auto resp = drogon::HttpResponse::newHttpJsonResponse(jsonResp);
+                callback(resp);
+        });
+
+    drogon::app().addListener("127.0.0.1", 8848);
+    drogon::app().run();
+    return 0;
+}
